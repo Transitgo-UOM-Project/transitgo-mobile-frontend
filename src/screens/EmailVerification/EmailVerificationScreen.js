9d9d1f3873ada3626ad5b6819f488c12a07cdf1b
ShabeebaBanu <banumrmrs.21@itfac.mrt.ac.lk>
2024-07-02 15:47:18 +0530

    user

---
+++ src/screens/EmailVerification/EmailVerificationScreen.js
@@ -0,0 +1,41 @@
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    useWindowDimensions,
+    ScrollView,
+    SafeAreaView,
+  } from "react-native";
+  import React from "react";
+ 
+  const  EmailVerificationScreen= () => {
+    
+  
+    return (
+      <View>
+        <SafeAreaView>
+          <View style={styles.root}>
+            <Text style={styles.title}>Please Check your Email to verify your Account</Text>
+            
+          </View>
+        </SafeAreaView>
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    root: {
+      alignItems: "center",
+      padding: 20,
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: "bold",
+      color: "#132968",
+      margin: 10,
+    },
+  });
+  
+  export default EmailVerificationScreen;
+  
