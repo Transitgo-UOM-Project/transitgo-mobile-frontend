5e7cb326209a34348cd6de206fbeb2c9cdc0ca8b
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2024-07-04 15:12:25 +0530

    User Profile Edit Issue Fixed

--- src/screens/ProfileScreen/Profile.js
+++ src/screens/ProfileScreen/Profile.js
@@ -1,12 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, ScrollView, Alert} from 'react-native';
-import CustomInput from '../../components/CustomInput/Index';
-import CustomButton from '../../components/CustomButton/Index';
-import { useNavigation} from "@react-navigation/native";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
-import Config from '@/config';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ScrollView, Alert } from "react-native";
+import CustomInputProfile from "../../components/CustomInputProfile/Index";
+import CustomButton from "../../components/CustomButton/Index";
+import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import Config from "@/config";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const apiURL = Config.API_BASE_URL;
 
@@ -14,20 +14,21 @@ const Profile = () => {
   const navigation = useNavigation();
   const [role, setRole] = useState("");
   const [isEditing, setIsEditing] = useState(false);
+  const [hasChanges, setHasChanges] = useState(false);
   const isPassenger = role === "passenger";
   const [profileInfo, setProfileInfo] = useState({
     fname: "",
     lname: "",
     email: "",
     uname: "",
-    id: ""
+    id: "",
   });
   const [editData, setEditData] = useState({
     fname: "",
     lname: "",
     uname: "",
+    email: "",
   });
-
 
   const handleEdit = () => {
     setIsEditing(true);
@@ -35,36 +36,41 @@ const Profile = () => {
       fname: profileInfo.fname,
       lname: profileInfo.lname,
       uname: profileInfo.uname,
+      email: profileInfo.email,
     });
   };
 
   const handleSave = async (e) => {
-    try{
-      const token = await AsyncStorage.getItem('token');
-      const response = await axios.put(`${apiURL}/admin-user/update/${profileInfo.id}`, editData, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
+    try {
+      const token = await AsyncStorage.getItem("token");
+      const response = await axios.put(
+        `${apiURL}/admin-user/update/${profileInfo.id}`,
+        editData,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
       console.warn("Saved");
       Alert.alert("Changed Saved");
 
       setProfileInfo(editData);
       setIsEditing(false);
-    }catch(error){
+    } catch (error) {
       console.warn(error);
       Alert.alert("Error Updating profile");
-    } 
+    }
   };
 
   const handleDelete = () => {
-     navigation.navigate("VerifyPassword");
-  }
+    navigation.navigate("VerifyPassword");
+  };
 
-  const getProfileInfo = async(e) => {
-    const token = await AsyncStorage.getItem('token');
-    const type = await AsyncStorage.getItem('role');
+  const getProfileInfo = async (e) => {
+    const token = await AsyncStorage.getItem("token");
+    const type = await AsyncStorage.getItem("role");
     setRole(type);
     console.log(type);
-    try{
+    try {
       const response = await axios.get(`${apiURL}/user/profile`, {
         headers: { Authorization: `Bearer ${token}` },
       });
@@ -75,7 +81,7 @@ const Profile = () => {
         uname: response.data.uname,
         id: response.data.id,
       });
-    }catch(error){
+    } catch (error) {
       console.warn("Error fetching data");
     }
   };
@@ -84,65 +90,76 @@ const Profile = () => {
     navigation.navigate("ConfirmEmail");
   };
 
-
   useEffect(() => {
     getProfileInfo();
-  },[]);
+  }, []);
 
   const onEditInput = (name, value) => {
-    setEditData({...editData, [name]: value});
+    setEditData({ ...editData, [name]: value });
+  };
+
+  const isSaveButtonVisible = () => {
+    return (
+      editData.fname !== profileInfo.fname ||
+      editData.lname !== profileInfo.lname ||
+      editData.uname !== profileInfo.uname
+    );
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.form}>
         <Text style={styles.label}>First Name</Text>
-        <CustomInput
+        <CustomInputProfile
           placeholder="First Name"
           value={isEditing ? editData.fname : profileInfo.fname}
           editable={isEditing}
-          onChangeText={text => onEditInput('fname', text)}
+          onChangeText={(text) => onEditInput("fname", text)}
         />
 
         <Text style={styles.label}>Last Name</Text>
-        <CustomInput
+        <CustomInputProfile
           placeholder="Last Name"
           value={isEditing ? editData.lname : profileInfo.lname}
           editable={isEditing}
-          onChangeText={text => onEditInput('lname', text)}
+          onChangeText={(text) => onEditInput("lname", text)}
         />
 
         <Text style={styles.label}>E-mail</Text>
-        <CustomInput
+        <CustomInputProfile
           placeholder="Email"
           value={profileInfo.email}
           editable={isEditing}
         />
 
         <Text style={styles.label}>User Name</Text>
-        <CustomInput
+        <CustomInputProfile
           placeholder="User Name"
           value={isEditing ? editData.uname : profileInfo.uname}
           editable={isEditing}
-          onChangeText={text => onEditInput('uname', text)}
+          onChangeText={(text) => onEditInput("uname", text)}
         />
-
-        <TouchableOpacity onPress={handlePasswordChange}>
-           <Text>Change Password</Text>
-        </TouchableOpacity>
+        {isEditing && (
+          <TouchableOpacity
+            onPress={handlePasswordChange}
+            style={styles.changePasswordContainer}
+          >
+            <Text style={styles.changePasswordText}>Change Password</Text>
+          </TouchableOpacity>
+        )}
 
         <View style={styles.buttonsContainer}>
           {!isEditing && <CustomButton text="Edit" onPress={handleEdit} />}
-          {isEditing && <CustomButton text="Save" onPress={handleSave} />}
-          {
-          isPassenger && 
-          <CustomButton 
-          text="Delete Account" 
-          onPress={handleDelete} />
-        }
+          {isEditing && isSaveButtonVisible() && (
+            <CustomButton text="Save" onPress={handleSave} />
+          )}
+          {isEditing && (
+            <CustomButton text="Discard" onPress={() => setIsEditing(false)} />
+          )}
+          {isPassenger && (
+            <CustomButton text="Delete Account" onPress={handleDelete} />
+          )}
         </View>
-
-       
       </View>
     </ScrollView>
   );
@@ -152,19 +169,25 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     padding: 20,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
   form: {
-    width: '100%',
+    width: "100%",
   },
   label: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginVertical: 10,
   },
   buttonsContainer: {
     marginTop: 20,
+  },
+  changePasswordContainer: {
+    alignSelf: "flex-end",
+  },
+  changePasswordText: {
+    color: "red",
   },
 });
 
